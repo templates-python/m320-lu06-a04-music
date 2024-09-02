893f9677e7dd5d03430a6d84f91fbeca77edec9a
Marcel Suter <marcel.suter@bzz.ch>
2024-09-02 08:01:54 +0200

    fixed method name

--- musician.py
+++ musician.py
@@ -23,9 +23,9 @@ class Musician:
         """
         return self._name
 
-    def play_tone(self, the_tone):
+    def play_note(self, the_tone):
         """
         Plays the musical note on the instrument.
         :param the_tone: der gespielte Ton
         """
-        print(f'{self._name} spielt mit {self._instrument.play_tone(the_tone)}')
+        print(f'{self._name} spielt mit {self._instrument.play_note(the_tone)}')
